bd2fe50721f1d989fcd832ca3fef92dcb37d3995
Stefan Couture <scouture@wawanesa.com>
2018-08-02 09:30:30 -0500

    Added folder structure and basics for website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Navbar from './js/components/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <Navbar></Navbar>
+    );
+  }
+}
+
+export default App;
