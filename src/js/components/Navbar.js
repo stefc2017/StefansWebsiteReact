cdd9bf98491ad630305d488e044d475a8f3471d2
Stefan Couture <scouture@wawanesa.com>
2018-08-02 10:10:43 -0500

    Removed unnecesary navbar menu items

--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -13,11 +13,6 @@ export default class Navbar extends Component {
       <div>
         <Menu pointing secondary>
           <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
-          <Menu.Menu position='right'>
-            <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
-          </Menu.Menu>
         </Menu>
 
         <Segment>
