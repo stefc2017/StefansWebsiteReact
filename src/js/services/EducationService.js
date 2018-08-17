b50a988a12162e98b2ec3b33e783c35ce90aad23
Stefan <stefc2013@gmail.com>
2018-08-16 20:04:14 -0500

    Finished

--- src/js/services/EducationService.js
+++ src/js/services/EducationService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 /**
- * Localhost: http://localhost:3000/api/education
- * AWS: add here
+ * Localhost: http://localhost:9000/api/education
+ * AWS: http://ec2-18-222-216-18.us-east-2.compute.amazonaws.com:9000/api/education
  */
 class EducationService{
     getEducationInfo(){
-        return axios.get('http://localhost:9000/api/education')
+        return axios.get('http://ec2-18-222-216-18.us-east-2.compute.amazonaws.com:9000/api/education')
     }
 }
 
