d19c1038c728b8837e9423b349dc2dbd4e8c785c
Stefan <stefc2013@gmail.com>
2018-08-21 23:20:43 -0500

    Fixed footer and icons

--- src/js/components/MyFooter/MyFooter.js
+++ src/js/components/MyFooter/MyFooter.js
@@ -1,20 +1,26 @@
 import React, {Component} from 'react';
 import {Footer} from 'react-materialize';
-
+import FontAwesome from 'react-fontawesome';
 import './MyFooter.css';
 
 class MyFooter extends Component{
     render(){
         return (
             <div>
-                <Footer copyrights="&copy; 2018 Stefan Couture. All rights reserved"
-                className='footer'
-                >
-                <div className="social-networks">
-                    <a href="https://github.com/stefc2017" className="github"><i className="fa fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/StefanVCouture" className="linkedIn"><i className="fa fa-linkedin"></i></a>
-                </div>
-                </Footer>
+                <footer class="page-footer footer">
+                    <div class="container">
+                        <div class="row">
+                            <a href="https://github.com/stefc2017" class="github"><FontAwesome name='github'/></a>
+                            <a href="https://www.linkedin.com/in/StefanVCouture" class="linkedIn"><FontAwesome name='linkedin'/></a>
+                            {/* </div> */}
+                        </div>
+                    </div>
+                    <div class="footer-copyright">
+                        <div class="container">
+                        Copyright © 2018 Stefan Couture. All rights reserved
+                        </div>
+                    </div>
+                </footer>
             </div>
         );
     }
