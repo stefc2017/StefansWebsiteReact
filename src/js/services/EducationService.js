2742af9a3f774fa808957d8c4ab98dc25ff8e16a
Ubuntu <ubuntu@ip-172-31-29-119.us-east-2.compute.internal>
2019-01-04 22:51:40 +0000

    Made changes to EducationService.js

--- src/js/services/EducationService.js
+++ src/js/services/EducationService.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 
-/**
- * Localhost: http://localhost:9000/api/education
- * AWS: http://stefanvcouture.ca:9000/api/education
- */
 class EducationService{
     getEducationInfo(){
-        return axios.get('http://stefanvcouture.ca:9000/api/education')
+        return axios.get('https://stefanvcouture.ca:9000/api/education')
     }
 }
 
